cd4f4826ddfa3c8de57340918fa35e8a9f94089a
EmilianStankov <eminstine@abv.bg>
2015-02-11 20:42:37 +0200

    Enable marrow editing

--- routes/marrows.rb
+++ routes/marrows.rb
@@ -14,3 +14,22 @@ get '/marrows/list_marrows' do
   @marrows = Marrows.where(creator: @@user)
   erb :'marrows/list_marrows'
 end
+
+get '/marrows/:marrow' do
+  authenticate
+  @marrow = Marrows.find_by(name: params[:marrow])
+  erb :'marrows/view_marrow'
+end
+
+get '/marrows/edit/:marrow' do
+  authenticate
+  @marrow = Marrows.find_by(name: params[:marrow])
+  erb :'marrows/edit_marrow'
+end
+
+post '/marrows/edit/:marrow' do
+  @marrow = Marrows.find_by(name: params[:marrow])
+  @marrow.update(content: params[:content])
+  @marrow.update(access_level: params[:access_level])
+  erb :'marrows/view_marrow'
+end
